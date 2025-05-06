d30aa08b2bd768a9dd5b76fd1fd5fb98aa53368b
Giovanna-Nascimento <giovanna.nascimento1511@gmail.com>
2025-05-06 02:02:47 -0300

    :sparkles: Alteração do arquivo .py e criação do arquivo .exe

--- Numeros_Impares.py
+++ Numeros_Impares.py
@@ -1,33 +1,41 @@
-#Escreva e demonstre (lógica / código / compilação / execução) um programa que receba uma sequência de números inteiros e retorne apenas os números ímpares.
+# Escreva e demonstre (lógica / código / compilação / execução) um programa que receba uma sequência de números inteiros e retorne apenas os números ímpares.
 
-
-# instanciamos uma lista, onde os números digitados serão armazenados 
-numeros = []
-
-# inicializamos o contador
-i = 1
-
-#título
+# ~ título ~
 print("\n")
 print("\t\t\tDETECTOR DE NÚMEROS ÍMPARES\n")
 
-# com a estrutura "while", condicionamos que o bloco de código se repita enquanto o contador for menor que 6, ou seja, 5 vezes
-while i < 6:
-    try:
-        x = int(input("Escreva um número inteiro: "))
-        # toda vez que esse bloco roda, o contador soma 1
-        i += 1
-        
-        # condição: se o resto da divisão do número entrado for igual a 1 (ou seja, se for ímpar), o número é adicionado à lista
-        if x % 2 == 1:
-            numeros.append(x)
+# estrutura de repetição para que o arquivo executável não se feche tão rapidamente
+while True:
+    # criamos uma lista, onde os números digitados serão armazenados 
+    numeros = []
 
-    # try except para detectar erros na entrada do usuário
-    except ValueError:
-        print("Erro: Por favor, digite um número inteiro.")
+    # inicializamos o contador
+    i = 1
 
-# condição que verifica se o tamanho da lista é igual a 0, isto é, se estiver vazia
-if len(numeros) == 0:
-    print("\nNão há números ímpares!\n")
-else:
-    print("\nNúmeros ímpares digitados: ", numeros, "\n")
+    print("\n")
+
+    # com outra estrutura while, fazemos com que o bloco de código se repita enquanto o contador for menor que 6, ou seja, 5 vezes
+    while i < 6:
+        try:
+            x = int(input("Escreva um número inteiro: "))
+            # toda vez que esse bloco roda, o contador soma 1
+            i += 1
+            
+            # condição: se o resto da divisão do número entrado for igual a 1 (ou seja, se for ímpar), o número é adicionado à lista
+            if x % 2 == 1:
+                numeros.append(x)
+
+        # try except para detectar erros na entrada do usuário
+        except ValueError:
+            print("Erro: Por favor, digite um número inteiro.")
+
+    # condição que verifica se o tamanho da lista é igual a 0, isto é, se estiver vazia
+    if len(numeros) == 0:
+        print("\nNão há números ímpares!\n")
+    else:
+        print("\nNúmeros ímpares digitados: ", numeros, "\n")
+
+    op = input("Deseja jogar novamente? [S/N] ").upper()
+    if op != 'S':
+        print("Obrigada por jogar!\n")
+        break
